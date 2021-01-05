ea4ec3fe691c279fd8a07adc7ad108daf6d76c13
Oliver J. Smith <smithol@oregonstate.edu>
2021-01-05 14:48:24 -0800

    Initial commit

---
+++ working_examples.py
@@ -0,0 +1,9 @@
+# Author: Oliver J. Smith
+# Date: 1/5/2021
+# Description: Working examples from the Think Python Book
+#              ...
+
+# Chapter 2 -- Variables, Expressions, and Statements
+miles = 26.2
+print("Miles: ", miles)
+print("KM: ", miles * 1.61)
